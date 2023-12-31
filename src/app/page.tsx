b584c603f9512d4d78d6b0daa606dc8d8b8eab01
MarcialFantini <marcialfantini@gmail.com>
2023-12-31 06:11:34 -0300

    nuevo avances mobile home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { AboutUs } from "@/components/AboutUs";
 import { HomeBanner } from "@/components/HomeBanner";
-import { SectionFooter } from "@/components/SectionFooter";
+
 import { SectionOurPrice } from "@/components/SectionOurPrice";
 import { SectionProcces } from "@/components/SectionProcces";
 import { SectionService } from "@/components/SectionService";
